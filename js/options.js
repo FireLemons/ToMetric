14155af6fb2e2c7a4a7c26871fdc20f7acd1ce04
FireLemons <shenyang64@gmail.com>
2019-09-19 12:44:00 -0500

    removed unconventional behavior

--- js/options.js
+++ js/options.js
@@ -2,74 +2,49 @@ var options = new Vue({
     el: "#options",
     computed: {
         distanceOn: {
-            get: function(){
-                var distanceOptions = this.options.conversions.distance;
-                
-                return checkConversionTypeEnabled(distanceOptions) && distanceOptions.on;
+            get: function() {
+                return this.options.conversions.distance.on
             },
             set: function(isEnabled){
                 this.options.conversions.distance.on = isEnabled;
                 this.SaveOptions();
             }
         },
-        distanceDisabled: function(){
-            return !checkConversionTypeEnabled(this.options.conversions.distance);
-        },
         liquidVolumeOn: {
             get: function(){
-                var liquidVolumeOptions = this.options.conversions.liquidVolume;
-                
-                return checkConversionTypeEnabled(liquidVolumeOptions) && liquidVolumeOptions.on;
+                return this.options.conversions.liquidVolume.on
             },
             set: function(isEnabled){
                 this.options.conversions.liquidVolume.on = isEnabled;
                 this.SaveOptions();
             }
         },
-        liquidVolumeDisabled: function(){
-            return !checkConversionTypeEnabled(this.options.conversions.liquidVolume);
-        },
         massOn: {
             get: function(){
-                var massOptions = this.options.conversions.mass;
-                
-                return checkConversionTypeEnabled(massOptions) && massOptions.on;
+                return this.options.conversions.mass.on
             },
             set: function(isEnabled){
                 this.options.conversions.mass.on = isEnabled;
                 this.SaveOptions();
             }
         },
-        massDisabled: function(){
-            return !checkConversionTypeEnabled(this.options.conversions.mass);
-        },
         speedOn: {
             get: function(){
-                var speedOptions = this.options.conversions.speed;
-                
-                return checkConversionTypeEnabled(speedOptions) && speedOptions.on;
+                this.options.conversions.speed.on
             },
             set: function(isEnabled){
                 this.options.conversions.speed.on = isEnabled;
                 this.SaveOptions();
             }
         },
-        speedDisabled: function(){
-            return !checkConversionTypeEnabled(this.options.conversions.speed);
-        },
         temperatureOn: {
             get: function(){
-                var temperatureOptions = this.options.conversions.temperature;
-                
-                return checkConversionTypeEnabled(temperatureOptions) && temperatureOptions.on;
+                return this.options.conversions.temperature.on
             },
             set: function(isEnabled){
                 this.options.conversions.temperature.on = isEnabled;
                 this.SaveOptions();
             }
-        },
-        temperatureDisabled: function(){
-            return !checkConversionTypeEnabled(this.options.conversions.temperature);
         }
     },
     data: {
@@ -165,24 +140,6 @@ var options = new Vue({
         this.SaveOptions();
     }
 });
-
-/*
- * Checks if all the metric units or all the customary units are off for a type of measurement
- *
- * param: optionsObject
- *      The object containing the toggled states of units
- *
- * return:
- *      true if at least one metric unit and one customary unit are enabled
- *      false otherwise
- */
-function checkConversionTypeEnabled(optionsObject){
-    
-    var isCustomaryEnabled = checkAnyEnabled(optionsObject.customary);
-    var isMetricEnabled = checkAnyEnabled(optionsObject.metric);
-    
-    return isCustomaryEnabled && isMetricEnabled;
-}
 
 /*
  * Checks if any units for a measurement system for a type of measurement are on
