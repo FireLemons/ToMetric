64c411e196c96cfca0b6508a1de4f2621718d85f
FireLemons <shenyang64@gmail.com>
2019-09-23 14:43:17 -0500

    basic scoring working

--- js/conversion.js
+++ js/conversion.js
@@ -1,13 +1,29 @@
-let conversionProblem = class{
+// Generates a TeX representation of a formula
+//   @param  {number} ratio The ratio for unit conversion
+//   @param  {string} imperial The abbreviation for the imperial unit of the conversion
+//   @param  {string} metric The abbreviation for the metric unit of the conversion
+//   @return {string} the TeX representation of the formula to convert from imperial to metric
+function generateFormula(ratio, imperial, metric){
+  let ratioAsSciNotation = ratio.toExponential()
+  let coefficientMagnitude = ratioAsSciNotation.match(/([0-9]\.[0-9]+)e([+-][0-9]+)/)
+  
+  if(coefficientMagnitude[2].charAt(0) === '+'){
+    coefficientMagnitude[2] = coefficientMagnitude[2].substr(1)
+  }
+  
+  return `1\\,\\mathrm{${imperial}} = ${coefficientMagnitude[1]}\\,\\mathrm{${metric}}` + (coefficientMagnitude[2] === '0' ? '' : `\\times 10^{${coefficientMagnitude[2]}}`)
+}
+
+let conversionProblemType = class{
   // @param {number} increment 
   // @param {number} ratio The ratio for unit conversion
   // @param {string} imperial The abbreviation for the imperial unit of the conversion
   // @param {string} metric The abbreviation for the metric unit of the conversion
-  // @param {string} formula The formula for conversion represented in TeX
   // @param {function} generateImperial(range) A function to generate imperial values for conversions
   //    @param {number} range A number to limit imperial value generated
   //    @return {number} A value in imperial units for conversion
-  constructor(ratio, imperial, metric, formula, generateImperial){
+  // @param {string=} formula(optional) The formula for conversion represented in TeX
+  constructor(ratio, imperial, metric, generateImperial, formula = ''){
     if(isNaN(ratio)){
       throw new TypeError('param ratio must be a number')
     }
@@ -31,7 +47,7 @@ let conversionProblem = class{
     this.ratio = ratio
     this.imperial = imperial
     this.metric = metric
-    this.formula = formula
+    this.formula = formula ? formula : generateFormula(ratio, imperial, metric);
     this.generateImperial = generateImperial
   }
   
@@ -48,42 +64,75 @@ let conversionProblem = class{
 }
 
 const conversionTypes = {
-  
+  inchesmillimeters: null,
+  inchescentimeters: null,
+  inchesmeters: new conversionProblemType(0.0254, 'in', 'm', (range) => {
+    return 20 + 10 * Math.ceil(Math.random() * range)
+  }),
+  incheskilometers: null,
+  feetmillimeters: null,
+  feetcentimeters: null,
+  feetmeters: null,
+  feetkilometers: null,
+  yardsmillimeters: null,
+  yardscentimeters: null,
+  yardsmeters: null,
+  yardskilometers: null,
+  milesmillimeters: null,
+  milescentimeters: null,
+  milesmeters: null,
+  mileskilometers: null
 }
+
+const config = JSON.parse(localStorage.getItem('ToMetric.Options'))
 
 let toMetric = new Vue({
   el: '#app',
   data: {
     score: 0,
+    errorPercent: undefined,
+    errorAmount: undefined,
+    exactConversionDisplay: undefined,
+    
+    given: 0,
+    imperialAbbrev: '',
     answerClass: 'grey darken-3',
     userAnswer: '',
-
-    conversionResult: undefined,
-    errorAmount: undefined,
-    errorPercent: undefined,
-    given: Math.ceil(Math.random() * 50),
-    tolerance: 0.1
+    exactConversion: 0,
+    metricAbbrev: '',
+    
+    tolerance: 1 / config.general.precision
   },
   methods: {
     checkAnswer: function () {
-      const exactConversion = this.given / 2.54
+      let exactConversion = this.exactConversion
 
       if (Math.abs(exactConversion - this.userAnswer) / exactConversion <= this.tolerance) {
-        this.given = Math.ceil(Math.random() * 50)
-        this.score += 1
-
-        M.toast({
-          html: 'What a great job you did!!!'
-        })
-
-        this.answerClass = 'grey darken-3'
-        this.answer = ''
+        this.onCorrect()
+        this.loadNewProblem()
       } else {
         this.answerClass = 'grey darken-3 wrong'
       }
+    },
+    loadNewProblem: function(){
+      this.answerClass = 'grey darken-3'
+      this.userAnswer = ''
+      
+      let problem = conversionTypes['inchesmeters']
+      
+      this.imperialAbbrev = problem.imperial
+      this.metricAbbrev = problem.metric
+      
+      let conversionPair = problem.generateConversionPair(10)
+      
+      this.given = conversionPair['imperial']
+      this.exactConversion = conversionPair['metric']
+    },
+    onCorrect: function(){
+      this.score += 1
     }
   },
   mounted: function () {
-
+    this.loadNewProblem()
   }
 })
