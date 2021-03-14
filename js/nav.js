5e55d92fd444d975d399e1899c6a21cf14acad33
firelemons <shenyang64@gmail.com>
2021-03-13 21:07:36 -0600

    fixed about not working in both pages, fixed tabbing in part

--- js/nav.js
+++ js/nav.js
@@ -1,18 +1,29 @@
 Vue.component("site-nav", {
-    template: `
-        <header>
-            <nav id="nav" class="grey darken-3">
-                <div class="nav-wrapper">
-                    <a href="./" class="brand-logo">ToMetric</a>
-                    <ul class="right">
-                        <li><a class="modal-trigger" href="#about-modal">About</a></li>
-                        <li><a class="" href="options.html"><i class="material-icons left">settings</i>Options</a></li>
-                    </ul>
-                </div>
-            </nav>
-        </header>`
-});
-
-new Vue({
-    el: '#navBar'
+  mounted: function() {
+    M.Modal.init(document.querySelectorAll('.modal'))
+    console.log("E")
+  },
+  template: `
+      <header>
+        <nav id="nav" class="grey darken-3">
+          <div class="nav-wrapper">
+            <a href="./" class="brand-logo">ToMetric</a>
+            <ul class="right">
+              <li><a class="modal-trigger" href="#about-modal">About</a></li>
+              <li><a class="" href="options.html"><i class="material-icons left">settings</i>Options</a></li>
+            </ul>
+          </div>
+        </nav>
+        <div id="about-modal" class="modal grey darken-3">
+          <div class="modal-content white-text">
+            <h4>About ToMetric</h4>
+            <p>A game for developing the ability to estimate the metric equivalent of a customary unit</p>
+            <p>Abandoned forever because metric system advocacy seems to attract strange political people</p>
+            <img src="./img/humanCommunication.png">
+          </div>
+          <div class="modal-footer grey darken-3">
+            <a href="#!" class="modal-close btn-flat white-text">Close</a>
+          </div>
+        </div>
+      </header>`
 });
